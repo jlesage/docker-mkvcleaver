8a91acdbfdfd988acb821ad60e36e106185247af
Jocelyn Le Sage <jocelyn.lesage@hpe.com>
2017-11-27 06:07:07 -0500

    Initial code drop.

---
+++ rootfs/etc/cont-init.d/mkvcleaver.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/with-contenv sh
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+
+# Make sure mandatory directories exist.
+mkdir -p /config/temp
+
+# Copy default configuration if needed.
+[ -f /config/custom.ini ] || cp /defaults/custom.ini /config/
+[ -f /config/user.reg ] || cp /defaults/user.reg /config/
+[ -f /config/system.reg ] || cp /defaults/system.reg /config/
+
+# Wine requires the WINEPREFIX directory to be owned by the user running the
+# Windows app.
+chown $USER_ID:$GROUP_ID /opt/mkvcleaver
+
+# Take ownership of the config directory.
+chown -R $USER_ID:$GROUP_ID /config
+
+# vim: set ft=sh :
